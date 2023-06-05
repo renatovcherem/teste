1c355a9e1863e7caeacbd30c5837f5db2f63b701
renatovcherem <135381517+renatovcherem@users.noreply.github.com>
2023-06-05 10:17:45 -0300

    nova pagina

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <html>
  <head>
+ 
   <title>Everest Digital</title>
  </head>
+ <style>
+body, html {
+  height: 80%;
+  margin: 0;
+}
+
+.bg {
+  /* The image used */
+  background-image: url("everest.png");
+
+  /* Full height */
+  height: 100%; 
+
+  /* Center and scale the image nicely */
+  #background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
  <body>
- <p>Olá Mundo</p>
+ <div class="bg"></div>
+ <p style="text-align:center; font-size:5vw;">Pagina de teste</p>
  </body>
 </html>
