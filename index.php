42744ff9db572cffddb77cd62ee17975a9720ae6
renatovcherem <135381517+renatovcherem@users.noreply.github.com>
2023-06-07 15:51:41 -0300

    teste 5

--- index.php
+++ index.php
@@ -24,6 +24,6 @@ body, html {
 </style>
  <body>
  <div class="bg"></div>
- <p style="text-align:center; font-size:5vw;">Pagina de teste3</p>
+ <p style="text-align:center; font-size:5vw;">Pagina de teste4</p>
  </body>
 </html>
